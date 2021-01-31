72618ad99012e0892d60cbd5078af4a7ee43df62
amaraa <monkhamar2@gmail.com>
2021-01-31 19:01:17 +0800

    ajil ed dundaa ywaj bna

---
+++ app.js
@@ -0,0 +1,75 @@
+// delgetstei ajillah controller
+var uiController = (function () {
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn" 
+    };
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+
+
+    };
+})();
+// Sanhuutei ajillah controller
+var financeController = (function () {
+    var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+var expense = function(){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+var data = {
+    allItems:{
+        inc:[],
+        exp:[]
+    },
+    totals:{
+        inc: 0,
+        exp: 0
+    }, 
+    
+}
+})();
+//Programmtai ajillah controller heseg
+var appController = (function (uiController, financeController) {
+    
+    var ctrlAddItem = function () {
+        console.log(uiController.getInput());
+    };
+   var setupEventListeners = function () {
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
+        ctrlAddItem();
+    });
+    document.querySelector("keypress", function (event) {
+        if (event.keyCode === 13 || event.wich === 13) {
+            ctrlAddItem();
+        }
+    });
+   }
+
+
+   return {
+       init: function(){
+           console.log ('Application started...');
+           setupEventListeners();
+       }
+   }
+
+})(uiController, financeController);
+appController.init();
